7373cd987c1b697a884dd8969db5f0a6d6905c9c
Akash Singh <goandsatish@gmail.com>
2019-11-23 13:58:54 +0530

    added alert dialog for removing cart item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -26,25 +26,42 @@ class CartItem extends StatelessWidget {
           color: Colors.white,
         ),
         alignment: Alignment.centerRight,
-        color: Theme
-            .of(context)
-            .errorColor,
+        color: Theme.of(context).errorColor,
       ),
-      onDismissed: (direction) => Provider.of<Cart>(context, listen: false).deleteItem(productId),
+      onDismissed: (direction) =>
+          Provider.of<Cart>(context, listen: false).deleteItem(productId),
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) {
+              return AlertDialog(
+                title: Text("Are you sure?"),
+                content: Text("Do you want to remove the item form the cart?"),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text("No"),
+                    onPressed: () => Navigator.of(context).pop(false),
+                  ),
+                  FlatButton(
+                    child: Text("Yes"),
+                    onPressed: () => Navigator.of(context).pop(true),
+                  )
+                ],
+              );
+            });
+      },
       child: Card(
         margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         child: ListTile(
           leading: CircleAvatar(
-            backgroundColor: Theme
-                .of(context)
-                .accentColor,
+            backgroundColor: Theme.of(context).accentColor,
             child: Padding(
               padding: const EdgeInsets.all(4.0),
               child: FittedBox(
                   child: Text(
-                    "\$$price",
-                    style: TextStyle(color: Colors.white),
-                  )),
+                "\$$price",
+                style: TextStyle(color: Colors.white),
+              )),
             ),
           ),
           title: Text(title),
