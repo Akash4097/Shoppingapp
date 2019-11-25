7577f00c3da5b8f572daa7856de5322e525800f3
Akash Singh <goandsatish@gmail.com>
2019-11-25 12:16:55 +0530

    added more textFormFields and imageurl getter

--- lib/screens/edit_products_screen.dart
+++ lib/screens/edit_products_screen.dart
@@ -9,6 +9,31 @@ class EditProductsScreen extends StatefulWidget {
 
 class _EditProductsScreenState extends State<EditProductsScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +58,46 @@ class _EditProductsScreenState extends State<EditProductsScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                maxLines: 3,
+                decoration: InputDecoration(labelText: "Description"),
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 8, right: 10),
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    child: FittedBox(
+                      child: _imageController.text.isEmpty
+                          ? Text("Enter Url")
+                          : Image.network(
+                              _imageController.text,
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: "Image Url"),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.url,
+                      focusNode: _imageUrlFocusNode,
+                      controller: _imageController,
+                    ),
+                  ),
+                ],
               )
             ],
           ),
