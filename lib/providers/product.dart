ebe3b3490caef1fc093ff99e7c3705dcc3dd1d79
Akash Singh <goandsatish@gmail.com>
2019-12-01 12:32:40 +0530

    added favorites based on user-part1

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -24,18 +24,18 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus(String token) async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
 
     final url =
-        "https://shoppingapp-e514f.firebaseio.com/products/$id.json?auth=$token";
+        "https://shoppingapp-e514f.firebaseio.com/userFavorites/$userId/$id.json?auth=$token";
     try {
-      final response = await http.patch(
+      final response = await http.put(
         url,
         body: json.encode(
-          {"isFavorite": isFavorite},
+          isFavorite,
         ),
       );
 
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,42 +12,45 @@ class ProductItem extends StatelessWidget {
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
     final auth = Provider.of<Auth>(context, listen: false);
-    return GridTile(
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        title: Text(product.title),
-        leading: Consumer<Product>(
-          builder: (ctx, product, child) => IconButton(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          title: Text(product.title),
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              color: Theme.of(context).accentColor,
+              onPressed: () => product.toggleFavoriteStatus(auth.token, auth.userId),
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            ),
+          ),
+          trailing: IconButton(
             color: Theme.of(context).accentColor,
-            onPressed: () => product.toggleFavoriteStatus(auth.token),
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("Added Item to the cart!"),
+                  action: SnackBarAction(
+                    label: "UNDO",
+                    onPressed: () => cart.removeSingleItem(product.id),
+                  ),
+                ),
+              );
+            },
+            icon: Icon(Icons.shopping_cart),
           ),
         ),
-        trailing: IconButton(
-          color: Theme.of(context).accentColor,
-          onPressed: () {
-            cart.addItem(product.id, product.price, product.title);
-            Scaffold.of(context).hideCurrentSnackBar();
-            Scaffold.of(context).showSnackBar(
-              SnackBar(
-                content: Text("Added Item to the cart!"),
-                action: SnackBarAction(
-                  label: "UNDO",
-                  onPressed: () => cart.removeSingleItem(product.id),
-                ),
-              ),
-            );
-          },
-          icon: Icon(Icons.shopping_cart),
-        ),
-      ),
-      child: GestureDetector(
-        onTap: () => Navigator.of(context)
-            .pushNamed(ProductDetailsScreen.routeName, arguments: product.id),
-        child: Image.network(
-          product.imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () => Navigator.of(context)
+              .pushNamed(ProductDetailsScreen.routeName, arguments: product.id),
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
