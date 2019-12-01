270bbf2958f6ee4d3e6b4da83711d91a321a0d92
Akash Singh <goandsatish@gmail.com>
2019-12-01 17:15:32 +0530

    done logout feature, added order based on userId

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,8 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProxyProvider<Auth, Orders>(
           // ignore: deprecated_member_use
-          builder: (ctx, auth, previousOrders) => Orders(
-              auth.token, previousOrders == null ? [] : previousOrders.orders),
+          builder: (ctx, auth, previousOrders) => Orders(auth.token,
+              auth.userId, previousOrders == null ? [] : previousOrders.orders),
         )
       ],
       child: Consumer<Auth>(
